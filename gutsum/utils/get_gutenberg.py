01ad5acbe8dd703cbf28f90ad84fec8d08996bc0
Ricardo G. Sousa <ricardo@rsousa.co>
2024-08-02 09:48:32 +0100

    feat: topic modeling for all jane austen books and corresponding results

--- gutsum/utils/get_gutenberg.py
+++ gutsum/utils/get_gutenberg.py
@@ -4,15 +4,19 @@ import gutenbergpy.textget
 
 def get_by_book_id(ids: list) -> pd.DataFrame:
 
-    books_by_id = {}
+    books_by_id = {'book': {}}
 
     for book_id in ids:
         raw_book = gutenbergpy.textget.get_text_by_id(book_id)
         clean_book = gutenbergpy.textget.strip_headers(raw_book)
 
-        books_by_id[book_id] = [clean_book]
+        books_by_id['book'].update({book_id: clean_book})
 
     # convert to pandas
     df = pd.DataFrame(books_by_id)
-    print(df)
     return df
+
+
+if __name__ == "__main__":
+    get_by_book_id([1342])
+
