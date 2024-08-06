73a02c99e6a4eba07a185e46a2db00863e42d25c
Ricardo G. Sousa <ricardo@rsousa.co>
2024-08-06 13:34:58 +0100

    feat: added route to request summaries from passages

--- booksum/service_route.py
+++ booksum/service_route.py
@@ -14,14 +14,31 @@ class BookSumSchema(BaseModel):
     book: str = Field(..., description="book title")
 
 
-def run_model(model, content) -> dict:
-    print(content)
+class BookTextSumSchema(BaseModel):
+    book_text: str = Field(..., description="book title")
+
+
+def run_model_given_book_title(model, content) -> dict:
     return model.summarize_given_book_title(content)
 
 
-async def run_predict(content: str, models: BookSumIndex) -> dict:
+async def summarize_given_book_title(content: str, models: BookSumIndex) -> dict:
     response = await run_in_threadpool(
-        run_model,
+        run_model_given_book_title,
+        models.model,
+        content
+    )
+
+    return response
+
+
+def run_model_given_book_text(model, content) -> dict:
+    return model.summarize_given_text(content)
+
+
+async def summarize_given_book_text(content: str, models: BookSumIndex) -> dict:
+    response = await run_in_threadpool(
+        run_model_given_book_text,
         models.model,
         content
     )
@@ -44,8 +61,37 @@ async def summarize(
     models = request.app.state.models
 
     try:
-        response = await run_predict(
+        response = await summarize_given_book_title(
             content=body.book,
+            models=models
+        )
+
+        return JSONResponse(response)
+
+    except Exception as ex:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=traceback.format_exc()
+        ) from ex
+
+
+@router.post(
+    "/summarize_text",
+    status_code=status.HTTP_200_OK,
+    summary="Summarize a book text.",
+    response_description="Returns the text summary."
+  )
+async def summarize(
+        request: Request,
+        body: BookTextSumSchema = Body(
+            ..., example={"book_text": "String"}
+        )
+):
+    models = request.app.state.models
+
+    try:
+        response = await summarize_given_book_text(
+            content=body.book_text,
             models=models
         )
 
